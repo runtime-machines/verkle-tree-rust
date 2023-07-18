05848ee1916006cb6b09b675cac84559e35a0d27
erikareale <13k.erika@gmail.com>
2023-07-18 12:16:18 +0200

    feat & fix: make generators global and correct logic relationship between commit-prove-verify

--- src/proving_schemes/mod.rs
+++ src/proving_schemes/mod.rs
@@ -1,19 +1,29 @@
 mod bulletproof;
 mod transcript_protocol;
 
-pub trait ProvingScheme<Point, CompressedPoint, Proof> {
-    /// Compute the commitment of a slice of bytes interpreted as little endian Scalars
-    fn compute_commitment(&self, bytes: &[[u8; 32]]) -> Self;
+pub const MAX_GENERATORS: usize = 256;
 
-    /// Return the commpressed commitment of the node
-    fn get_commitment(&self) -> CompressedPoint;
+pub trait ProvingScheme<Node, CompressedPoint, Scalar, Proof> {
+    // Instantiate the `ProvingScheme` instantiating a starting amount of generators
+    fn instantiate_generators() -> Self;
+
+    // Increase the generators' quantity
+    fn add_new_generator(&self);
+
+    /// Generate a polynomial and its commitment from slice of points
+    fn compute_commitment(&self, bytes: &[[u8; 32]]) -> Node;
 
     /// Convert a compressed commitment in a byte array
     fn commitment_to_bytes(com: CompressedPoint) -> [u8; 32];
 
-    /// Compute the proof of a node
-    fn prove<Protocol>(&self) -> Proof;
+    /// Compute the proof that the point (position, evaluation) is a node's child
+    fn prove<Protocol>(&self, node: &Node, point: &(u64, [u8; 32])) -> Proof;
 
-    /// Verify the proof of a node
-    fn verify(proof: &Proof, children: &[CompressedPoint]) -> bool;
+    /// Verify that points the point (position, evaluation) is a node's child
+    fn verify(
+        &self,
+        proof: &Proof,
+        children_count: usize,
+        point: &(u64, [u8; 32]),
+    ) -> bool;
 }
