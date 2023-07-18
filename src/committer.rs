5f0e26915b9ee24d52c56818c8145f61044ec508
bonomip <paolo.bonomi@outlook.com>
2023-07-18 10:20:09 +0200

    WIP

--- src/committer.rs
+++ src/committer.rs
@@ -1,10 +1,11 @@
 pub trait Committer {
     type Commit;
+    type Scalar;
 
     //number of bytes should be less then prover max filed exponent
-    fn from_bytes_to_scalar(&self, input: &[u8]) -> &[u8];
+    fn from_bytes_to_scalar(&self, input: &[u8]) -> &Self::Scalar;
 
-    fn commit(&self, input: Vec<&[u8]>) -> &Self;
+    fn commit(&self, input: Vec<&Self::Scalar>) -> &Self;
 
     fn consume_commit(&self) -> Self::Commit;
 
