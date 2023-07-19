346364c752d78eb89882cc6ef35c5f7e313bd1bc
bonomip <paolo.bonomi@outlook.com>
2023-07-19 09:30:43 +0200

    WIP

--- src/committer.rs
+++ src/committer.rs
@@ -1,13 +1,14 @@
 pub trait Committer {
-    type Commit;
+    type Point;
     type Scalar;
 
     //number of bytes should be less then prover max filed exponent
     fn from_bytes_to_scalar(&self, input: &[u8]) -> &Self::Scalar;
+    fn from_point_to_scalar(&self, input: &Self::Point) -> &Self::Scalar;
 
     fn commit(&self, input: Vec<&Self::Scalar>) -> &Self;
 
-    fn consume_commit(&self) -> Self::Commit;
+    fn consume_commit(&self) -> Self::Point;
 
     fn prove(&self) -> ();
 
--- src/node.rs
+++ src/node.rs
@@ -24,102 +24,122 @@ pub struct LeafNode {
     pub suffix: KeySuffix, //for left from 0 to 127, for right from 128 to 255
 
     pub value: LeafNodeValue,
-    pub modifier: bool,
-    /*
-        // lower++upper (without considering modifier) = leaf_value
-        pub lower: LowerValue, // + 1 bit (129th bit) to differentiate between a leaf that
-        // has never been accessed and a leaf that has been overwritten with 0s
-        // 1=accessed 0=never accessed
-        pub upper: UpperValue,
+    pub lower: LowerValue,
+    pub upper: UpperValue,
+    pub modifier: bool, //this could be removed
+                        /*
+                            // lower++upper (without considering modifier) = leaf_value
+                            pub lower: LowerValue, // + 1 bit (129th bit) to differentiate between a leaf that
+                            // has never been accessed and a leaf that has been overwritten with 0s
+                            // 1=accessed 0=never accessed
+                            pub upper: UpperValue,
 
-    */
+                        */
 }
 
 impl LeafNode {
     pub fn new(suffix: KeySuffix, value: LeafNodeValue) -> Self {
+        let modifier = false;
+        let (lower, upper) = Self::split_node_value(value, modifier);
+
         LeafNode {
             suffix,
             value,
-            modifier: false,
+            modifier,
+            lower,
+            upper,
         }
     }
 
     pub fn update(mut self, value: LeafNodeValue) -> () {
         self.modifier = true;
         self.value = value;
+
+        let (lower, upper) = Self::split_node_value(self.value, self.modifier);
+
+        self.lower = lower;
+        self.upper = upper;
     }
 
-    //TODO: check if its better to pre calulate it or
-    //to call it when needed
-    fn split_value(&self) -> Vec<&[u8]> {
+    fn split_node_value(
+        value: LeafNodeValue,
+        modifier: bool,
+    ) -> (LowerValue, UpperValue) {
         let mut lower: LowerValue = Default::default();
         lower[0..LEAF_HALF_VALUE_SIZE + 1]
-            .copy_from_slice(&self.value[0..LEAF_HALF_VALUE_SIZE + 1]);
+            .copy_from_slice(&value[0..LEAF_HALF_VALUE_SIZE + 1]);
         lower[LEAF_HALF_VALUE_SIZE] = {
-            if self.modifier {
+            if modifier {
                 1
             } else {
                 0
             }
         };
 
-        let upper: UpperValue = self.value
-            [LEAF_HALF_VALUE_SIZE..LEAF_VALUE_SIZE]
+        let upper: UpperValue = value[LEAF_HALF_VALUE_SIZE..LEAF_VALUE_SIZE]
             .try_into()
             .expect("Node value has not 32 bytes");
 
-        vec![lower.as_slice(), upper.as_slice()]
+        (lower, upper)
     }
 }
 
 pub struct SuffixTree<C: Committer> {
     // C1 = Commit(v0_lower_modifier, v0_upper, ..., v127_lower_modifier, v127_upper)
-    pub c1: C::Commit,
+    pub c1: C::Point,
     pub left_branch: SuffixTreeBranch,
     // C2 = Commit(v128_lower_modifier, v128_upper, ..., v255_lower_modifier, v255_upper)
-    pub c2: C::Commit,
+    pub c2: C::Point,
     pub right_branch: SuffixTreeBranch,
 }
 
 impl<C: Committer> SuffixTree<C> {
-    pub fn compute_c_1(mut self, committer: C) {
+    pub fn compute_c(mut self, committer: &C) {
+        //this two operation can be done concurrently
         self.c1 = self.compute_commit(&self.left_branch, committer);
-    }
-
-    pub fn compute_c_2(mut self, committer: C) {
         self.c2 = self.compute_commit(&self.right_branch, committer);
     }
 
     fn compute_commit(
         &self,
         branch: &SuffixTreeBranch,
-        committer: C,
-    ) -> C::Commit {
-        committer.commit(Self::pack_leaves(branch)).consume_commit()
+        committer: &C,
+    ) -> C::Point {
+        committer
+            .commit(
+                Self::pack_leaves(branch)
+                    .iter()
+                    .map(|i| committer.from_bytes_to_scalar(i))
+                    .collect(),
+            )
+            .consume_commit()
     }
 
-    pub fn pack_leaves(branch: &SuffixTreeBranch) -> Vec<&[u8]> {
-        branch.iter().map(|n| n.split_value()).flatten().collect()
+    fn pack_leaves(branch: &SuffixTreeBranch) -> Vec<&[u8]> {
+        branch
+            .iter()
+            .map(|n| vec![n.lower.as_slice(), n.upper.as_slice()])
+            .flatten()
+            .collect()
     }
 }
 pub struct ExtensionNode<C: Committer> {
     pub stem: KeyStem,
     //C = Commit(1, stem, C1, C2)
-    pub c: C::Commit,
+    pub c: C::Point,
     pub suffix_tree: SuffixTree<C>,
 }
 
 impl<C: Committer> ExtensionNode<C> {
-    pub fn compute_c(&self, committer: C) -> () {
-        let m_stem = committer.from_bytes_to_scalar(&self.stem);
-        let one = committer.from_bytes_to_scalar(&[1]);
-
-        committer.commit(vec![
-            one,
-            m_stem,
-            &self.suffix_tree.c1,
-            &self.suffix_tree.c2,
-        ]);
+    pub fn compute_c(&mut self, committer: &C) -> () {
+        self.c = committer
+            .commit(vec![
+                committer.from_bytes_to_scalar(&[1]),
+                committer.from_bytes_to_scalar(&self.stem),
+                committer.from_point_to_scalar(&self.suffix_tree.c2),
+                committer.from_point_to_scalar(&self.suffix_tree.c2),
+            ])
+            .consume_commit();
     }
 }
 
